13792d972c7e2f44b85dfcfea4012c29969ba0ae
Smit90 <45687440+Smit90@users.noreply.github.com>
2023-05-23 11:11:41 +0530

    shopping-list form using template-driven approach

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -5,6 +5,7 @@ import { Subject } from "rxjs";
 export class ShoppingListService {
 
     ingredientChanged = new Subject<Ingredient[]>()
+    startedEditing = new Subject<number>()
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -18,8 +19,22 @@ export class ShoppingListService {
         return this.ingredients.slice();
     }
 
+    getIngredient(index: number) {
+        return this.ingredients[index]
+    }
+
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientChanged.next(this.ingredients.slice())
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient
+        this.ingredientChanged.next(this.ingredients.slice())
+    }
+
+    deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1)
         this.ingredientChanged.next(this.ingredients.slice())
     }
 
