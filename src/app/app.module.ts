5c133f54a218e0ec54574210ced429137f99f3ce
Smit Patel <45687440+Smit90@users.noreply.github.com>
2023-05-29 06:51:30 +0530

    adding an auth feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AuthComponent } from './auth/auth/auth.component';
 import { RecipeModule } from './recipes/recipe.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RecipeModule,
     ShoppingListModule,
+    AuthModule,
     SharedModule,
     CoreModule
   ],
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { AuthComponent } from "./auth/auth.component";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { SharedModule } from "../shared/shared.module";
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+    declarations: [
+        AuthComponent,
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        SharedModule,
+        RouterModule.forChild(
+            [
+                {
+                    path: 'auth',
+                    component: AuthComponent,
+                }
+            ]
+        )
+    ]
+})
+export class AuthModule {
+
+}
